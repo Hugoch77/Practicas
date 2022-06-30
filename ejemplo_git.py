346bacc3311c5f3224eeb7af29d0c57ec28d22cd
Hugo <hug_alb_97otmail.com>
2022-06-29 20:44:55 -0500

    ejemplo.git numeros primos

--- ejemplo_git.py
+++ ejemplo_git.py
@@ -1,1 +1,13 @@
-# nuevo para git
+def isprime(num):
+    cont = 0
+    for i in range(1, num+1):
+        if num % i == 0:
+            cont += 1
+    if cont > 2:
+        return False
+    else:
+        return True
+
+for i in range(2, 1001):
+    if isprime(i):
+        print(f"El numero {i} es Primo")
